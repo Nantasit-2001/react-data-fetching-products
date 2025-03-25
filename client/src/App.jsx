3710aa73e167e1b64f46b69e72b1a0a6adec46d1
nantasit <nantasitnsboss@gmail.com>
2025-03-25 13:48:53 +0700

    feat:Pull data from the server and Render it.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,29 +1,44 @@
 import "./App.css";
+import axios from "axios";
+import { useState,useEffect } from "react";
 
 function App() {
+  const [productData,setProductData] = useState([])
+  
+  useEffect(()=>{getProductData();},[]);
+
+  async function getProductData (){
+    const response = await axios.get("http://localhost:4001/products")
+    setProductData(response.data.data);
+    console.log(response.data.data)
+  }
+
   return (
     <div className="App">
       <div className="app-wrapper">
         <h1 className="app-title">Products</h1>
       </div>
       <div className="product-list">
-        <div className="product">
+        {productData.map(objProductData=>{return(
+        
+        <div className="product" key={objProductData.id}>
           <div className="product-preview">
             <img
-              src="https://via.placeholder.com/350/350"
+              src={objProductData.image}
               alt="some product"
               width="350"
               height="350"
             />
           </div>
-          <div className="product-detail">
-            <h1>Product name: ...</h1>
-            <h2>Product price: ... Baht</h2>
-            <p>Product description: .....</p>
-          </div>
-
-          <button className="delete-button">x</button>
+              <div className="product-detail">
+                <h1>{objProductData.name}</h1>
+                <h2>{objProductData.price}</h2>
+                <p>{objProductData.description}</p>
+              </div>
+              <button className="delete-button">x</button> 
         </div>
+      
+      )})}
       </div>
     </div>
   );
